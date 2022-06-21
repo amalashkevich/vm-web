3d44b97f0b377c658d125adf6a5337d6f30eaf3f
Alexey Malashkevich <am@agilecode.io>
2022-06-21 10:37:13 -0400

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 mod utils;
-
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -22,4 +21,59 @@ pub fn submit_code(byte_code: &str) {
     let result = run_machine(byte_code);
     println!("Result={:?}", result);
     alert(&format!("Result is {}", result));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vm_ta::run_machine;
+
+    #[test]
+    fn test_bytecode1() {
+        let byte_code = "
+            LOAD_VAL 1
+            WRITE_VAR 'x'
+
+            LOAD_VAL 2
+            WRITE_VAR 'y'
+
+            READ_VAR 'x'
+            LOAD_VAL 1
+            ADD
+
+            READ_VAR 'y'
+            MULTIPLY
+
+            RETURN_VALUE
+        ";
+
+    let result = run_machine(byte_code);
+    assert_eq!(result, "4");
+    }
+
+    #[test]
+    fn test_bytecode2() {
+        let byte_code = "
+            LOAD_VAL 2
+            WRITE_VAR 'x'
+
+            LOAD_VAL 8
+            WRITE_VAR 'y'
+
+            READ_VAR 'x'
+            LOAD_VAL 1
+            ADD
+
+            READ_VAR 'y'
+            MULTIPLY
+
+            READ_VAR 'y'
+            MULTIPLY
+
+            RETURN_VALUE
+            ";
+
+    let result = run_machine(byte_code);
+    assert_eq!(result, "192");
+    }
+
 }
