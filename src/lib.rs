ad5e1fd7f2e84203b30607502630a417c72d27f3
Alexey Malashkevich <am@agilecode.io>
2022-06-22 22:42:38 -0400

    Added support for - SEND_CHANNEL - RECV_CHANNEL - SPAWN - while loop

--- src/lib.rs
+++ src/lib.rs
@@ -20,12 +20,14 @@ use crate::vm_ta::run_machine;
 pub fn submit_code(byte_code: &str) {
     let result = run_machine(byte_code);
     println!("Result={:?}", result);
-    alert(&format!("Result is {}", result));
+    alert(&format!("Result is {}", result.unwrap()));
 }
 
 #[cfg(test)]
 mod tests {
     use crate::vm_ta::run_machine;
+    use std::thread;
+    use std::time::Duration;
 
     #[test]
     fn test_bytecode1() {
@@ -46,8 +48,8 @@ mod tests {
             RETURN_VALUE
         ";
 
-    let result = run_machine(byte_code);
-    assert_eq!(result, "4");
+        let result = run_machine(byte_code);
+        assert_eq!(result.unwrap(), "4");
     }
 
     #[test]
@@ -72,8 +74,77 @@ mod tests {
             RETURN_VALUE
             ";
 
-    let result = run_machine(byte_code);
-    assert_eq!(result, "192");
+        let result = run_machine(byte_code);
+        assert_eq!(result.unwrap(), "192");
     }
 
+    #[test]
+    fn test_loop() {
+        // x = 0
+        // while x < 3 {
+        //   print x
+        //   x = x + 1
+        // }
+        let byte_code = "
+            LOAD_VAL 0
+            WRITE_VAR 'x'
+
+            LABEL :loop1start
+            READ_VAR 'x'
+            LOAD_VAL 3
+            CMP_LT
+            POP_JUMP_IF_FALSE :loop1end
+
+            READ_VAR 'x'
+            PRINT 'x'
+
+            READ_VAR 'x'
+            LOAD_VAL 1
+            ADD
+            WRITE_VAR 'x'
+
+            JUMP :loop1start
+
+            LABEL :loop1end
+            READ_VAR 'x'
+            RETURN_VALUE
+        ";
+        let result = run_machine(byte_code);
+        assert_eq!(result.unwrap(), "3");
+    }
+
+    #[test]
+    fn test_send_channel() {
+        let byte_code = "
+            PUSH 'val'
+            PUSH 'channel_name'
+            SEND_CHANNEL
+            ";
+
+        run_machine(byte_code);
+    }
+
+    #[test]
+    fn test_recv_channel() {
+        let byte_code = "
+            PUSH 'channel_name'
+            RECV_CHANNEL
+            RETURN_VALUE
+            ";
+
+        let result = run_machine(byte_code);
+        assert_eq!(result.unwrap(), "recv_channel_value");
+    }
+
+    #[test]
+    fn test_spawn() {
+        let byte_code = "
+            PUSH 'channel_recv'
+            PUSH 'channel_send'
+            SPAWN
+            ";
+
+        run_machine(byte_code);
+        thread::sleep(Duration::from_millis(15));
+    }
 }
--- src/vm_ta.rs
+++ src/vm_ta.rs
@@ -9,6 +9,7 @@ type Result<T> = std::result::Result<T, String>;
 pub enum Operand {
     I(i64),
     S(String),
+    B(bool)
 }
 
 impl Operand {
@@ -23,6 +24,13 @@ impl Operand {
         match self {
             Operand::S(ref s) => Some(s),
             _ => None,
+        }
+    }
+
+    fn to_bool(&self) -> Option<bool> {
+        match self {
+            Operand::B(b) => Some(*b),
+            _ => None
         }
     }
 }
@@ -45,12 +53,20 @@ impl<'a> From<&'a str> for Operand {
     }
 }
 
+impl From<bool> for Operand {
+    fn from(b: bool) -> Self {
+        Operand::B(b)
+    }
+}
+
+
 impl fmt::Display for Operand {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         //write!(f,"{:?}", self)
         match self {
             Operand::I(i) => write!(f, "{}", i),
             Operand::S(s) => write!(f, "{}", s),
+            Operand::B(b) => write!(f, "{}", b),
         }
     }
 }
@@ -92,6 +108,80 @@ fn ret(machine: &mut Machine<Operand>, _args: &[usize]) {
     machine.ret();
 }
 
+fn cmp_lt(machine: &mut Machine<Operand>, _args: &[usize]) {
+    let rhs = machine.operand_pop().clone();
+    let lhs = machine.operand_pop().clone();
+    let result = lhs.to_i().unwrap() < rhs.to_i().unwrap();
+    machine.operand_push(Operand::from(result));
+}
+
+fn pop_jump_if_false(machine: &mut Machine<Operand>, args: &[usize]) {
+    let condition = machine.operand_pop().to_bool().unwrap();
+    if !condition {
+        let label = machine.get_data(args[0]).clone();
+        machine.jump(label.to_s().unwrap());
+    }
+}
+
+fn print_(machine: &mut Machine<Operand>, args: &[usize]) {
+    let val = machine.operand_pop().clone();
+
+    print!("{}", val.to_string());
+}
+
+fn jump(machine: &mut Machine<Operand>, args: &[usize]) {
+    let label = machine.get_data(args[0]).clone();
+    machine.jump(label.to_s().unwrap());
+}
+
+fn push(machine: &mut Machine<Operand>, args: &[usize]) {
+    let arg = machine.get_data(args[0]).clone();
+    machine.operand_push(arg);
+}
+
+fn send_channel(machine: &mut Machine<Operand>, args: &[usize]) {
+    let ch = machine.operand_pop().clone();
+    let val = machine.operand_pop().clone();
+    println!("SEND (ch={}, val={})", ch, val);
+}
+
+fn recv_channel(machine: &mut Machine<Operand>, args: &[usize]) {
+    let ch = machine.operand_pop().clone();
+    println!("RECV (ch={})", ch);
+    let val: &str = "recv_channel_value";
+    machine.operand_push(Operand::from(val));
+}
+
+use std::thread;
+use std::time::Duration;
+
+fn spawn(machine: &mut Machine<Operand>, args: &[usize]) {
+    let func1 = machine.operand_pop().clone();
+    let func2 = machine.operand_pop().clone();
+    println!("SPAWN (func1={}, func2={})", func1, func2);
+
+    thread::spawn(|| {
+        fn func1() {
+            for i in 1..10 {
+                println!("spawned func1 i={}", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+            println!("spawned func1 end");
+        }
+        func1();
+    });
+    thread::spawn(|| {
+        fn func2() {
+            for i in 1..10 {
+                println!("spawned func2 i={}", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+            println!("spawned func2 end");
+        }
+        func2();
+    });
+}
+
 fn prepare_instruction_table() -> InstructionTable<Operand> {
     let mut instruction_table = InstructionTable::new();
     instruction_table.insert(Instruction::new(0, "LOAD_VAL", 1, load_val));
@@ -100,6 +190,15 @@ fn prepare_instruction_table() -> InstructionTable<Operand> {
     instruction_table.insert(Instruction::new(3, "ADD", 0, add));
     instruction_table.insert(Instruction::new(4, "MULTIPLY", 0, multiply));
     instruction_table.insert(Instruction::new(5, "RETURN_VALUE", 0, ret));
+    instruction_table.insert(Instruction::new(6, "CMP_LT", 0, cmp_lt));
+    instruction_table.insert(Instruction::new(7, "POP_JUMP_IF_FALSE", 1, pop_jump_if_false));
+    instruction_table.insert(Instruction::new(8, "PRINT", 1, print_));
+    instruction_table.insert(Instruction::new(9, "JUMP", 1, jump));
+
+    instruction_table.insert(Instruction::new(10, "PUSH", 1, push));
+    instruction_table.insert(Instruction::new(11, "SEND_CHANNEL", 0, send_channel));
+    instruction_table.insert(Instruction::new(12, "RECV_CHANNEL", 0, recv_channel));
+    instruction_table.insert(Instruction::new(13, "SPAWN", 0, spawn));
     return instruction_table;
 }
 
@@ -120,14 +219,19 @@ fn parse_byte_code<'a>(byte_code: &'a str,
             2 => {
                 let instr = items[0].trim();
                 let arg = items[1].trim();
-                let test = &arg.parse::<i64>();
-                match test {
-                    Ok(num) => {
-                        builder.push(instr, vec![Operand::from(num)]);
-                    }
-                    Err(_) => {
-                        let trimmed = arg.trim_matches('\'');
-                        builder.push(instr, vec![Operand::from(trimmed)]);
+
+                if instr == "LABEL" {
+                    builder.label(arg);
+                } else {
+                    let test = &arg.parse::<i64>();
+                    match test {
+                        Ok(num) => {
+                            builder.push(instr, vec![Operand::from(num)]);
+                        }
+                        Err(_) => {
+                            let trimmed = arg.trim_matches('\'');
+                            builder.push(instr, vec![Operand::from(trimmed)]);
+                        }
                     }
                 }
             }
@@ -139,7 +243,7 @@ fn parse_byte_code<'a>(byte_code: &'a str,
     return Ok(builder);
 }
 
-pub fn run_machine(byte_code: &str) -> String {
+pub fn run_machine(byte_code: &str) -> Option<String> {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     let instruction_table = prepare_instruction_table();
     let constants: WriteManyTable<Operand> = WriteManyTable::new();
@@ -148,11 +252,50 @@ pub fn run_machine(byte_code: &str) -> String {
             let mut machine: Machine<Operand> = Machine::new(Code::from(builder),
                                                      &constants, &instruction_table);
             machine.run();
-            let result = machine.operand_pop();
-            return result.to_string();
+            if machine.operand_stack.is_empty() {
+                return None;
+            }
+            return Some(machine.operand_pop().to_string());
         },
         Err(err) => {
-            return err;
+            return Some(err);
         }
     }
+}
+
+
+pub fn ex_main() {
+    let instruction_table = prepare_instruction_table();
+    let mut builder: Builder<Operand> = Builder::new(&instruction_table);
+
+    builder.push("LOAD_VAL", vec![Operand::from(0)]);
+    builder.push("WRITE_VAR", vec![Operand::from("x")]);
+
+    builder.label(":loop1start");
+    builder.push("READ_VAR", vec![Operand::from("x")]);
+    builder.push("LOAD_VAL", vec![Operand::from(3)]);
+    builder.push("CMP_LT", vec![]);
+    builder.push("POP_JUMP_IF_FALSE", vec![Operand::from(":loop1end")]);
+
+    builder.push("READ_VAR", vec![Operand::from("x")]);
+    builder.push("PRINT", vec![Operand::from("x")]);
+
+    builder.push("READ_VAR", vec![Operand::from("x")]);
+    builder.push("LOAD_VAL", vec![Operand::from(1)]);
+    builder.push("ADD", vec![]);
+    builder.push("WRITE_VAR", vec![Operand::from("x")]);
+
+    builder.push("JUMP", vec![Operand::from(":loop1start")]);
+    builder.label(":loop1end");
+    builder.push("RETURN_VALUE", vec![]);
+
+
+    let constants: WriteManyTable<Operand> = WriteManyTable::new();
+
+    let mut machine: Machine<Operand> = Machine::new(Code::from(builder), &constants, &instruction_table);
+    machine.run();
+
+    let result = machine.operand_pop();
+    print!("Result={}", result);
+    assert_eq!(result, Operand::from(4));
 }
